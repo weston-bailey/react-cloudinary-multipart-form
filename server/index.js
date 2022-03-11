92d59555b843982db4c634e4b46e17aeb423c21b
weston-bailey <wdbailey87@gmail.com>
2022-03-10 17:45:04 -0800

    uplouads to cloudinary working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 // required packages
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
-const { readdirSync } = require('fs')
+const { unlinkSync } = require('fs')
 const multer = require('multer')
+const cloudinary = require('cloudinary').v2
 
 // config express app
 const app = express()
@@ -18,19 +20,18 @@ app.use((req, res, next) => {
 
 const uploads = multer({ dest: 'uploads/' })
 
-
 app.get('/', (req, res) => {
   res.json({ msg: 'welcome to the image upload API 👋' })
 })
 
-app.post('/image', uploads.single('image'), (req, res) => {
-  const files = readdirSync('uploads')
-
-  console.log(files)
-
-  if (files.length === 0) return res.json({ msg: 'no file uploaded! ' })
-
-  res.json({ files })
+app.post('/image', uploads.single('image'), async (req, res) => {
+  if (!req.file) return res.json({ msg: 'no file uploaded! ' })
+ process.env.CLOUDINARY_URL
+  const test = await  cloudinary.uploader.upload(req.file.path)
+  console.log(test)
+  // remove the file after finishing up with it
+  unlinkSync(req.file.path)
+  res.json({ msg: 'file upload successfully!' })
 })
 
 app.listen(PORT, () => console.log(`listening to smooth sounds of port ${PORT} in the morning 🌊`))
